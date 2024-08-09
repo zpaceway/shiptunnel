2e163f3963718168094b38f8a8c3f349847e0295
Alexandro Tapia <zpaceway@gmail.com>
2024-08-09 17:04:53 -0500

    remove unused server variables

--- src/server/environment.ts
+++ src/server/environment.ts
@@ -3,7 +3,6 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const environment = {
-  SHIPTUNNEL_SERVER_HOST: process.env["SHIPTUNNEL_SERVER_HOST"]!,
   SHIPTUNNEL_SERVER_PORT: parseInt(process.env["SHIPTUNNEL_SERVER_PORT"]!),
   SHIPTUNNEL_SERVER_MAX_CONNECTION_RETRY_ATTEMPTS: parseInt(
     process.env["SHIPTUNNEL_SERVER_MAX_CONNECTION_RETRY_ATTEMPTS"]!
