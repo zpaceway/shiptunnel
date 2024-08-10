d54a7eef1c24a44d3322ef9ba2b39d8c16eeb826
Alexandro Tapia <zpaceway@gmail.com>
2024-08-10 00:57:55 -0500

    fix response

--- src/constants.ts
+++ src/constants.ts
@@ -9,15 +9,20 @@ export const MESSAGES = {
 
 export const HTTP_END_TEXT = "\r\n\r\n";
 
-export const HTTP_500_RESPONSE =
-  `HTTP/1.1 500 Internal Server Error\r\n` +
-  `Content-Type: text/html; charset=UTF-8\r\n` +
-  `Content-Length: 137\r\n` +
-  `Connection: close\r\n\r\n` +
+const HTTP_500_RESPONSE_BODY =
   `<html>\r\n` +
   `<head><title>500 Internal Server Error</title></head>\r\n` +
   `<body>\r\n` +
   `<h1>Internal Server Error</h1>\r\n` +
   `<p>An unexpected error occurred.</p>\r\n` +
   `</body>\r\n` +
-  `</html>\r\n\r\n`;
+  `</html>\r\n`;
+
+const contentLength = Buffer.byteLength(HTTP_500_RESPONSE_BODY, "utf-8");
+
+export const HTTP_500_RESPONSE =
+  `HTTP/1.1 500 Internal Server Error\r\n` +
+  `Content-Type: text/html; charset=UTF-8\r\n` +
+  `Content-Length: ${contentLength}\r\n` +
+  `Connection: close\r\n\r\n` +
+  HTTP_500_RESPONSE_BODY;
