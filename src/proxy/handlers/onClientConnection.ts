70b8406e534e3d2d1a4dc1808ac727afbb71356c
Alexandro Tapia <zpaceway@gmail.com>
2024-09-05 02:05:07 -0500

    finally

--- src/proxy/handlers/onClientConnection.ts
+++ src/proxy/handlers/onClientConnection.ts
@@ -29,7 +29,6 @@ const onClientConnection = (clientSocket: net.Socket) => {
       `PROXY: Left available tunnels for ${host}: ${tunnels[host]?.length || 0}`
     );
 
-    tunnelSocket.emit("unavailable");
     tunnelSocket.write(data);
     tunnelSocket.pipe(clientSocket, { end: true });
     clientSocket.pipe(tunnelSocket, { end: true });
