d324587a6c41efe54f410bfa093c60d3c2deb8b2
Alexandro Tapia <zpaceway@gmail.com>
2024-09-05 02:08:11 -0500

    add custom event

--- src/proxy/handlers/onTunnelConnection.ts
+++ src/proxy/handlers/onTunnelConnection.ts
@@ -11,36 +11,38 @@ const onTunnelConnection = (tunnelSocket: net.Socket) => {
       tunnelSocket.end();
     }, 20000);
 
-    ["data", "end", "close", "timeout", "error"].forEach((event) => {
-      tunnelSocket.on(event, () => {
-        clearTimeout(connectionTimeout);
-        const initialSize = tunnels[subdomain]?.length || 0;
-        tunnels[subdomain] = tunnels[subdomain]?.filter((socket) => {
-          if (socket === tunnelSocket) {
-            if (event !== "data") {
-              tunnelSocket.end();
+    ["data", "end", "close", "timeout", "error", "unavailable"].forEach(
+      (event) => {
+        tunnelSocket.on(event, () => {
+          clearTimeout(connectionTimeout);
+          const initialSize = tunnels[subdomain]?.length || 0;
+          tunnels[subdomain] = tunnels[subdomain]?.filter((socket) => {
+            if (socket === tunnelSocket) {
+              if (event !== "data") {
+                tunnelSocket.end();
+              }
+              logger.log(
+                `PROXY: Tunnel removed because of event ${event}: ${
+                  tunnels[subdomain]?.length || 0
+                }`
+              );
+              return false;
             }
+
+            return true;
+          });
+
+          const newSize = tunnels[subdomain]?.length || 0;
+
+          if (newSize !== initialSize)
             logger.log(
-              `PROXY: Tunnel removed because of event ${event}: ${
+              `PROXY: Available tunnels for ${subdomain}: ${
                 tunnels[subdomain]?.length || 0
               }`
             );
-            return false;
-          }
-
-          return true;
         });
-
-        const newSize = tunnels[subdomain]?.length || 0;
-
-        if (newSize !== initialSize)
-          logger.log(
-            `PROXY: Available tunnels for ${subdomain}: ${
-              tunnels[subdomain]?.length || 0
-            }`
-          );
-      });
-    });
+      }
+    );
 
     logger.log(
       `PROXY: New tunnel connected to handle requests from ${subdomain}`
