99395a3cb5a147659ccdf951fb1f30f2431cfcc4
Alexandro Tapia <zpaceway@gmail.com>
2024-09-08 06:39:24 -0500

    destroy on end

--- src/communication/index.ts
+++ src/communication/index.ts
@@ -2,12 +2,18 @@ import net from "net";
 
 export const bindSokets = (socket1: net.Socket, socket2: net.Socket) => {
   socket1.on("close", () => socket2.end());
-  socket1.on("end", () => socket2.end());
+  socket1.on("end", () => {
+    socket1.destroy();
+    socket2.end();
+  });
   socket1.on("error", () => socket2.end());
   socket1.on("timeout", () => socket2.end());
 
   socket2.on("close", () => socket1.end());
-  socket2.on("end", () => socket1.end());
+  socket2.on("end", () => {
+    socket2.destroy();
+    socket1.end();
+  });
   socket2.on("error", () => socket1.end());
   socket2.on("timeout", () => socket1.end());
 
