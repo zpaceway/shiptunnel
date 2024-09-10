07e9636c377226934bac74dce8ef4d225574248b
Alexandro Tapia <zpaceway@gmail.com>
2024-09-09 19:03:46 -0500

    use different bind

--- src/communication/index.ts
+++ src/communication/index.ts
@@ -1,19 +1,19 @@
 import net from "net";
 
 export const bindSokets = (socket1: net.Socket, socket2: net.Socket) => {
-  // [
-  //   [socket1, socket2],
-  //   [socket2, socket1],
-  // ].forEach(([s1, s2]) => {
-  //   s1!.on("close", () => s2!.end());
-  //   s1!.on("error", () => s2!.end());
-  //   s1!.on("timeout", () => s2!.end());
-  //   s1!.on("data", (data) => s2!.write(data));
-  //   s1!.on("end", () => {
-  //     s1!.destroy();
-  //     s2!.end();
-  //   });
-  // });
-  socket1.pipe(socket2, { end: true });
-  socket2.pipe(socket1, { end: true });
+  [
+    [socket1, socket2],
+    [socket2, socket1],
+  ].forEach(([s1, s2]) => {
+    s1!.once("close", () => s2!.end());
+    s1!.once("error", () => s2!.end());
+    s1!.once("timeout", () => s2!.end());
+    s1!.on("data", (data) => s2!.write(data));
+    s1!.once("end", () => {
+      s1!.destroy();
+      s2!.end();
+    });
+  });
+  // socket1.pipe(socket2, { end: true });
+  // socket2.pipe(socket1, { end: true });
 };
