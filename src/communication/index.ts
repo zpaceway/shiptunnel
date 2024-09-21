b0d7fc67563cfda1ed85e929381df470d44998e6
Alexandro Tapia <zpaceway@gmail.com>
2024-09-21 04:02:36 -0500

    update

--- src/communication/index.ts
+++ src/communication/index.ts
@@ -4,6 +4,6 @@ import { TunnelSocket } from "../proxy/types";
 
 export const bindSokets = (socket1: net.Socket, socket2: TunnelSocket) => {
   const socket2Duplex = createWebSocketStream(socket2);
-  socket1.pipe(socket2Duplex);
-  socket2Duplex.pipe(socket1);
+  socket1.pipe(socket2Duplex, { end: true });
+  socket2Duplex.pipe(socket1, { end: true });
 };
