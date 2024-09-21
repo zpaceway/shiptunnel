effb6651056c4935d2a312abc3e08c7411d777a3
Alexandro Tapia <zpaceway@gmail.com>
2024-09-21 03:41:24 -0500

    use duplex

--- src/communication/index.ts
+++ src/communication/index.ts
@@ -1,14 +1,9 @@
 import net from "net";
+import { createWebSocketStream } from "ws";
 import { TunnelSocket } from "../proxy/types";
 
 export const bindSokets = (socket1: net.Socket, socket2: TunnelSocket) => {
-  socket1.once("end", () => socket2.close());
-  socket1.once("error", () => socket2.close());
-  socket1.once("close", () => socket2.close());
-  socket1.once("timeout", () => socket2.close());
-  socket1.on("data", (data) => socket2.send(data));
-
-  socket2.once("error", () => socket1.end());
-  socket2.once("close", () => socket1.end());
-  socket2.on("message", (message) => socket1.write(message as Buffer));
+  const socket2Duplex = createWebSocketStream(socket2);
+  socket1.pipe(socket2Duplex);
+  socket2Duplex.pipe(socket1);
 };
